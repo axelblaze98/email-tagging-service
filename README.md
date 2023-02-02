c7d38fd163375028495c4a019f4033ec8ab2c7ac
axelblaze98 <paarthbhandari98@gmail.com>
2023-02-02 13:31:30 +0530

    Code refactoring and bug Fixes With addition of docker file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # email-tagging-service
+
+Generate a password from https://security.google.com/settings/security/apppasswords and use that password instead
+
+ADD .env file
+
+EMAIL= Your Gmail Id
+PASSWORD= Your password generated from above link
+IMAP_HOST=imap.gmail.com
+IMAP_PORT=993
+SMTP_HOST=smtp.gmail.com
+SMTP_PORT=587
+
+Run Server Commands
+
+Build Container
+
+docker build -t 'container name in lowercase' .
+
+Run Container
+
+docker run -it 'container name in lowercase' // to see logs
+docker run -d 'container name in lowercase' // to run in detached mode
--- index.js
+++ index.js
@@ -1,106 +1,112 @@
-const Imap = require('imap');
-const simpleParser = require('mailparser').simpleParser;
-const nodemailer = require('nodemailer');
+const Imap = require('imap')
+const simpleParser = require('mailparser').simpleParser
+const nodemailer = require('nodemailer')
+require('dotenv').config();
 
-// Generate a password from https://security.google.com/settings/security/apppasswords and use that password instead.
 const imap = new Imap({
-  user: 'paarthbhandaritest123@gmail.com',
-  password: 'jvablsnpnbmzbpsc',
-  host: 'imap.gmail.com',
-  port: 993,
+  user: process.env.EMAIL,
+  password: process.env.PASSWORD,
+  host: process.env.IMAP_HOST,
+  port: process.env.IMAP_PORT,
   tls: true,
   tlsOptions: { rejectUnauthorized: false },
-});
+})
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
   secure: false,
   auth: {
-    user: 'paarthbhandaritest123@gmail.com',
-    pass: 'jvablsnpnbmzbpsc',
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
-});
+})
+
+function random(min, max) {  
+  return Math.floor(
+    Math.random() * (max - min + 1) + min
+  )
+}
+  
 
 function checkForEmails() {
   imap.once('ready', function () {
     imap.openBox('INBOX', false, function (err, box) {
-      if (err) throw err;
-
-      // *  const searchCriteria = ['UNSEEN', ['SINCE', 'January 1st, 2023']];
+      if (err) throw err
 
       imap.search(['UNSEEN'], function (err, results) {
-        if (err) throw err;
+        if (err) throw err
 
-        var f = imap.fetch(results, {
-          bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-          struct: true,
-        });
+        let unseenMails
+        try {
+            unseenMails = imap.fetch(results, {
+            bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+            struct: true,
+            })
+        } catch (err) {
+            console.log(err.message)
+        }
         // * reading mail
-        f.on('message', function (msg, seqno) {
-          // console.log('Message #%d', seqno);
-          // var prefix = '(#' + seqno + ') ';
+        if (unseenMails) {
+            unseenMails.on('message', function (msg, seqno) {
+              msg.on('body', async (stream, info) => {
+                const mail = await simpleParser(stream)
 
-          // * send reply
-          msg.on('body', async (stream, info) => {
-            const mail = await simpleParser(stream);
+                const reply = {
+                  from: process.env.EMAIL,
+                  to: mail.from.text,
+                  subject: 'Re: ' + mail.subject,
+                  text: 'Thank you for your email.',
+                }
+                transporter.sendMail(reply, (err, info) => {
+                  if (err) throw err;
+                    console.log('Reply sent to ' + mail.from.text + '.')
+                  })
+              })
 
-            const reply = {
-              from: 'paarthbhandaritest123@gmail.com',
-              to: mail.from.text,
-              subject: 'Re: ' + mail.subject,
-              text: 'Thank you for your email.',
-            };
-            transporter.sendMail(reply, (err, info) => {
-              if (err) throw err;
-              console.log('Reply sent to ' + mail.from.text + '.');
-            });
-          });
+              //  * Add the "Replies" label to the email
+              // Chnage array string to change lable
+              imap.addLabels(seqno, ['Replies'], function (err) {
+                if (err) console.log(err)
+                else console.log('Label added')
+              })
+              // * setting seen flag
+              imap.setFlags(results, ['\\Seen'], (err) => {
+                if (err) throw err
+                  console.log(`Marked ${results.length} emails as seen.`)
+                  imap.end()
+              })
+            })
+            unseenMails.once('error', function (err) {
+              console.log('Fetch error: ' + err)
+            })
+            unseenMails.once('end', function () {
+              console.log('Done fetching all messages!')
+              imap.end()
+            })
+        } else {
+            imap.end()
+        }
+      })
+    })
+  })
 
-          // msg.on('body', function (stream, info) {
-          //   console.log(prefix + 'Body');
-          //   stream.pipe(process.stdout);
-          // });
-          // msg.once('attributes', function (attrs) {
-          //   console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-          // });
-          // msg.once('end', function () {
-          //   console.log(prefix + 'Finished');
-          // });
-
-          //  * Add the "Replies" label to the email
-          imap.addLabels(seqno, ['Replies'], function (err) {
-            if (err) console.log(err);
-            else console.log('Label added');
-          });
-          // * setting seen flag
-          imap.setFlags(results, ['\\Seen'], (err) => {
-            if (err) throw err;
-            console.log(`Marked ${results.length} emails as seen.`);
-            imap.end();
-          });
-        });
-        f.once('error', function (err) {
-          console.log('Fetch error: ' + err);
-        });
-        f.once('end', function () {
-          console.log('Done fetching all messages!');
-          imap.end();
-        });
-      });
-    });
-  });
-
-  imap.once('error', function (err) {
-    console.log(err);
-  });
+  imap.on('error', function (err) {
+    console.log(err)
+  })
 
   imap.once('end', function () {
-    console.log('Connection ended');
-  });
+    console.log('Connection ended')
+  })
 
-  imap.connect();
+  imap.connect()
 }
 
-// setInterval(checkForEmails, 60000);
-checkForEmails();
+setInterval(function () {
+  try {
+      console.log('Server Started')
+      checkForEmails()
+  } catch (ex) {
+    console.log({ ex })
+  }
+}, random(45000, 120000))
